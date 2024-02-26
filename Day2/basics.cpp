02030f3c68141f19314fa251306b0c29db8dff36
Anantha Narayanan <ananthanarayanan431@gmail.com>
2024-02-26 19:34:08 +0530

    Day 2 program

---
+++ Day2/basics.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+
+    int arr[num];
+    for (int i=0; i<num; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<arr[0]<<endl;
+    return 0;
+}
